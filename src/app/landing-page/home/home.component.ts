bdfafd00085d3a94b81b584a129425b62612ab1f
mygoodkab <mygood_kab@hotmail.com>
2018-07-11 19:24:18 +0700

    update home

--- src/app/landing-page/home/home.component.ts
+++ src/app/landing-page/home/home.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  img_slide = [
+    { img: '../../../assets/images/21.jpg' },
+    { img: '../../../assets/images/21.jpg' },
+    { img: '../../../assets/images/21.jpg' },
+  ];
+
+  new_arrival = [
+    { img: '../../../assets/images/1.jpg', detail_short: '32 inch with Crystal', name: 'Pearl', pro_id: 'PR0005' },
+    { img: '../../../assets/images/2.jpg', detail_short: '16 inch with Ruby Pendant', name: 'Pearl', pro_id: 'PR0001' },
+    { img: '../../../assets/images/3.jpg', detail_short: 'lenght 90 cm', name: 'Pearl', pro_id: 'SC0005' },
+  ];
+
+  products = [
+    { img: '../../../assets/images/1.jpg', name: 'Jewelry' },
+    { img: '../../../assets/images/2.jpg', name: 'Jewelry' },
+    { img: '../../../assets/images/3.jpg', name: 'Scarf' },
+    { img: '../../../assets/images/4.jpg', name: 'Wallet' },
+    { img: '../../../assets/images/5.jpg', name: 'Perfume' },
+    { img: '../../../assets/images/6.jpg', name: 'Hangbag' },
+  ];
 
   constructor() { }
 
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +8,48 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Sticky Header
+    const stick_header = document.getElementById('stick-header');
+    const logo = document.getElementById('logo');
+    const sticky = stick_header.offsetTop;
+    const window_scroll = document.body.scrollTop + document.documentElement.scrollTop;
+
+    // Scroll Down
+    if (window_scroll < 120) { // 120 ความยาวของ header
+      stick_header.classList.remove('stick-header');
+      logo.classList.remove('blank-menu');
+    } else {
+      stick_header.classList.add('stick-header');
+      logo.classList.add('blank-menu');
+
+    }
+    // if (sticky >= window_scroll) {
+    //   console.log('Gooooooooooooooooooooooood');
+    //   stick_header.classList.remove('stick-header');
+    //   logo.classList.remove('blank-menu');
+    // }
+
+    console.log('Sticky : ', sticky);
+    console.log('Window_scroll : ', window_scroll);
+    // if (sticky - window_scroll < 61) {
+    //   stick_header.classList.remove('stick-header');
+    //   logo.classList.remove('blank-menu');
+    // }
+    // if (sticky - 100 < window_scroll) {
+    //   stick_header.classList.remove('stick-header');
+    //   logo.classList.remove('blank-menu');
+
+    // } else {
+    //   stick_header.classList.add('stick-header');
+    //   logo.classList.add('blank-menu');
+
+    // }
+
+
+  }
 
   ngOnInit() {
   }
